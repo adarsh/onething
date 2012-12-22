728f331cfdfc81ad1927c07598e039a8b031a91e
Adarsh Pandit <adarsh@thoughtbot.com>
2012-12-22 10:49:52 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onething::Application.config.secret_token = '7cd2be4fdc3c71ab5e7c0c67061868db82ca3e5cda7d8a350943b33fc1e2fe1d54de6cd6f5db3f239b098d2ae784002c576cf590f6328519ed18adee223be607'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Onething::Application.routes.draw do
+  root :to => 'Clearance::Sessions#new'
+
+end
